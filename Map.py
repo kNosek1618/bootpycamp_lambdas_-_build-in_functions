3c18213b1e5f48a3f4195e700179579c8df3451f
toor <k.nosek@ievent-management.pl>
2019-07-22 19:46:00 +0000

    THE SAME RESULT WITH USING 'MAP' and 'DEF' function

---
+++ Map.py
@@ -0,0 +1,34 @@
+
+# THE SAME RESULT WITH USING 'MAP' and 'DEF' function
+
+nums = (2,4,6,8,10)
+
+doubles = map(lambda x: x*2, nums)
+print(list(doubles))
+
+##################################
+
+def check(*args):
+    score = []
+    for a in args:
+        a *= 2
+        score.append(a)
+    return score
+
+print(check(*nums))
+
+
+
+###################################
+
+# def cube(*args):
+#     score = 0
+#     for arg in args:
+#         score += arg
+#     return score
+#
+# nums = (2,4,6,8,10)
+# print(cube(*nums))
+
+
+# doubles = map(lambda x: x*2 ,nums)
---
+++ test1.py
@@ -0,0 +1,10 @@
+
+stop = 70
+score = 0
+
+for i in range(2,stop+1,):
+    score += i
+
+print(score)
+
+
